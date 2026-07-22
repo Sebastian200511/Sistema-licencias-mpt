425ead5248111ea1a0636aaac21a60fd93b06c06
Sebastian200511 <paredesriveramatias@gmail.com>
2026-07-22 13:05:14 -0500

    fix: hide continue form if RUC has an expired license

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -71,6 +71,7 @@ export default function Login() {
         const vencida = empresaDb.expedientes.find(exp => exp?.estado === 'Vencido');
         if (vencida) {
           setError(`El RUC ingresado cuenta con una licencia VENCIDA (Código: ${vencida.codigo}). Por favor, utilice la opción "Consultar el estado de mi trámite" que está arriba para ingresar a su panel y solicitar la Renovación.`);
+          setEmpresaValidada(null);
           setBuscandoSunat(false);
           return;
         }
